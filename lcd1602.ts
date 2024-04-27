9f13170eba9e7d968e72b77acf1c4f1cd4843792
ouger10086 <64428792+ouger10086@users.noreply.github.com>
2024-04-27 18:15:49 +0800

    更新 pxt.json, lcd1602.ts, test.ts

--- lcd1602.ts
+++ lcd1602.ts
@@ -1,7 +1,7 @@
 // MakerBit blocks supporting an I2C LCD 1602 display
 
 const enum LcdPosition1602 {
-  //% block="1"
+  //% block="1" 
   Pos1 = 1,
   //% block="2"
   Pos2 = 2,
@@ -71,73 +71,24 @@ const enum LcdPosition1602 {
 //% category="MakerBit"
 namespace makerbit {
 
-  /**
-   * Displays a text on a LCD1602 in the given position range.
-   * The text will be cropped if it is longer than the provided length.
-   * If there is space left, it will be filled with pad characters.
-   * @param text the text to show, eg: "MakerBit"
-   * @param startPosition the start position on the LCD, [1 - 32]
-   * @param length the maximum space used on the LCD, eg: 16
-   * @param option configures padding and alignment, eg: TextOption.Left
-   */
-  //% subcategory="LCD"
-  //% blockId="makerbit_lcd_show_string_on_1602"
-  //% block="LCD1602 show %text | at position %startPosition=makerbit_lcd_position_1602 with length %length || and %option"
-  //% text.shadowOptions.toString=true
-  //% length.min=1 length.max=32 length.fieldOptions.precision=1
-  //% expandableArgumentMode="toggle"
-  //% inlineInputMode="inline"
-  //% weight=90
-  export function showStringOnLcd1602(
-    text: string,
-    startPosition: number,
-    length: number,
-    option?: TextOption
-  ): void {
-    updateCharacterBuffer(
-      text,
-      startPosition - 1,
-      length,
-      16,
-      2,
-      toAlignment(option),
-      toPad(option)
-    );
-  }
 
-  /**
-   * Clears the LCD1602 completely.
-   */
-  //% subcategory="LCD"
-  //% blockId="makerbit_lcd_clear_1602" block="LCD1602 clear display"
-  //% weight=89
-  export function clearLcd1602(): void {
-    showStringOnLcd1602("", 1, 32);
-  }
 
-  /**
-   * Turns a LCD position into a number.
-   * @param pos the LCD position, eg: LcdPosition1602.Pos1
-   */
-  //% subcategory="LCD"
-  //% blockId=makerbit_lcd_position_1602
-  //% block="%pos"
-  //% pos.fieldEditor="gridpicker"
-  //% pos.fieldOptions.columns=16
-  //% blockHidden=true
-  export function position1602(pos: LcdPosition1602): number {
-    return pos;
-  }
 
-  /**
-   * Display a custom character at a specified LCD position.
-   */
-  //% subcategory="LCD"
-  //% blockId="makerbit_lcd_showchararacter1602"
-  //% block="LCD1602 show character %char|at position %position=makerbit_lcd_position_1602"
-  //% weight=58
-  export function lcdShowCharacter1602(char: LcdChar, position: number): void {
-    setCharacter(char, position - 1, 16, 2);
-  }
+
+
+
+    /**
+     * Turns a LCD position into a number.
+     * @param pos the LCD position, eg: LcdPosition1602.Pos1
+     */
+    //% subcategory="LCD"
+    //% blockId=makerbit_lcd_position_1602
+    //% block="%pos"
+    //% pos.fieldEditor="gridpicker"
+    //% pos.fieldOptions.columns=16
+    //% blockHidden=true
+    export function position1602(pos: LcdPosition1602): number {
+        return pos;
+    }
 
 }
